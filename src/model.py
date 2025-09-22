119c5c247dd65c263324cf6a8f2d80d1cfebc3c9
Pamelathanasoulias <pamela.athanasoulias@outlook.com>
2025-09-21 20:13:37 -0400

    STEP 6B: MODELS ADDED | ARIMA & EXPONENTIAL

--- src/model.py
+++ src/model.py
@@ -1,21 +1,49 @@
-# DARTS ARIMA STARTER (SIMPLE, BEGINNER-FRIENDLY)
-
-from darts.models import ARIMA
+# MODEL | ARIMA
+from typing import Dict, Any
 from darts import TimeSeries
+from darts.models import ARIMA
+from darts.models import ExponentialSmoothing
+from darts.metrics import smape
 
-class TimeSeriesModel:
-    # WRAPPER AROUND A SIMPLE ARIMA MODEL
 
-    def __init__(self, series: TimeSeries):
-        # SAVE THE INPUT SERIES
-        self.series = series
-        self.model = None
+class TheModel:
+    def __init__(self, config: Dict[str, Any]):
+        self.config = config
+        self.arima = None
 
-    def fit_arima(self, order=(2, 1, 1)):
-        # FIT ARIMA WITH DEFAULT ORDER
-        self.model = ARIMA(order=order)
-        self.model.fit(self.series)
+    def arima_model(self) -> ARIMA:
+        p, d, q = self.config.get("the_arima_order", [1, 1, 1])
+        self.arima = ARIMA(order=(p, d, q))
+        return self.arima
 
-    def predict(self, steps: int = 300) -> TimeSeries:
-        # PREDICT NEXT STEPS (DEFAULT 300)
-        return self.model.predict(steps)
+    def arima_fit(self, train_y: TimeSeries) -> None:
+        if self.arima is None:
+            self.arima_model()
+        self.arima.fit(train_y)
+
+    def arima_predict(self, horizon: int) -> TimeSeries:
+        return self.arima.predict(horizon)
+
+    def arima_score(self, y_true: TimeSeries, y_pred: TimeSeries) -> float:
+        return float(smape(y_true, y_pred))
+    
+
+class TheSecondModel:
+    def __init__(self, config: Dict[str, Any]):
+        self.config = config
+        self.exponential = None
+
+    def exponential_model(self) -> ExponentialSmoothing:
+        self.exponential = ExponentialSmoothing()
+        return self.exponential
+
+    def exponential_fit(self, train_y: TimeSeries) -> None:
+        if self.exponential is None:
+            self.exponential_model()
+        self.exponential.fit(train_y)
+
+    def exponential_predict(self, horizon: int) -> TimeSeries:
+        return self.exponential.predict(horizon)
+
+    def exponential_score(self, y_true: TimeSeries, y_pred: TimeSeries) -> float:
+        return float(smape(y_true, y_pred))
