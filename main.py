eb65042a5784f7fc40b4bc2139ebc0e84522d23e
Pamelathanasoulias <pamela.athanasoulias@outlook.com>
2025-09-23 09:30:31 -0400

    main ADJUSTMENTS

--- main.py
+++ main.py
@@ -8,32 +8,32 @@ def the_main():
     # CONFIG
     params = YAMLLoader().load_file("params.yaml")
 
-    # DATA SOURCE | CLEAN DATASET ONLY (FULL PATH)
+    # DATA SOURCE | CLEAN DATASET
     data_path = "/Users/pam/Desktop/MYPROJECTS/FINAL_PROJECT/data/processed/CLEAN_WEATHER.csv"
     print(f"# DATA SOURCE | CLEAN -> {data_path}")
 
     # LOAD CLEAN DATA
     df = CSVLoader().load_file(data_path)
 
-    # TRANSFORM | ENSURE COLUMNS OK -> SERIES -> SPLIT
+    # TRANSFORM | DARTS SERIES
     the_transform = WeatherTransform(df, params)
-    the_transform.the_columns()          # CLEAN COLUMNS
+    the_transform.the_columns()
     the_transform.build_series()
     the_train, the_test = the_transform.train_test_split()
 
-    # MODELS | ARIMA & LINEAR | MLFLOW LOGGING
+    # MODELS | ARIMA & LINEAR
     the_models = WeatherModels(
         the_train,
         the_test,
         the_transform.covariates_series,
-        params
-    )
+        params)
+    
     model_dictionary = the_models.run_the_models()
 
-    # REPORT | PLOTS + METRICS CSV
+    # REPORT | PLOTS & METRICS
     df_metrics = the_report(the_train, the_test, model_dictionary, out_dir="docs")
 
-    # SHOW METRICS
+    # THE RESULTS
     print(df_metrics)
 
 
